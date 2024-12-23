3a29dc403d540dcaa16b55b5e55dcee43bed8066
Jeko <jeko.erg@gamil.com>
2024-12-23 17:05:46 +0400

    cc

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://guess.testpuzzle.ge:8080/"
+    origins "http://guess.testpuzzle.ge"
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+             expose: ['Authorization']
   end
 end
